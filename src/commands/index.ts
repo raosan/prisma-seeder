99148151f5df8c08fb66c2294128f72f7169e4ff
raosan <raosanf.lillahi@gmail.com>
2022-10-20 22:47:47 +0700

    generate and read from json file

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,5 +1,6 @@
 import {Command, Flags} from '@oclif/core'
-
+import {execSync} from 'node:child_process'
+import * as fs from 'node:fs'
 export default class PrismaSeeder extends Command {
   static description = 'Start seeding by providing path_to_schema_prisma_file and url_of_the_database'
 
@@ -18,6 +19,22 @@ Finish seeding.
   async run(): Promise<void> {
     const {flags} = await this.parse(PrismaSeeder)
 
+    this.log('Start seeding...')
+    this.log('============================')
     this.log(`The schema path: ${flags.schema} \nand the DB url: ${flags['database-url']}`)
+    this.log('============================')
+
+    // generate json file
+    execSync(`npx prisma generate --schema=${flags.schema}`, {encoding: 'utf-8'})  // the default is 'buffer'
+
+    // read from json file
+    const text = fs.readFileSync('json-schema/json-schema.json', 'utf8')
+    const jsonFile = JSON.parse(text)
+    const models = Object.keys(jsonFile.definitions)
+    console.log(models.length)
+    console.log(models)
+
+    this.log('============================')
+    this.log('Finish seeding.')
   }
 }
