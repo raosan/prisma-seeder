5b89c90510804c32f76f72c0acfde1435727a95e
raosan <raosanf.lillahi@gmail.com>
2022-10-28 14:45:38 +0700

    success hardcode seed

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -40,67 +40,65 @@ Finish seeding.
     )
     this.log('============================')
 
+    // update env DB
+    execSync('cp ./.env.example ./.env', { encoding: 'utf-8' })
+    updateEnv('./.env', 'DATABASE_URL', flags['database-url'])
+
+    const schemaFlags = `--schema=${flags.schema}`
     // generate json file
-    execSync(`npx prisma generate --schema=${flags.schema}`, {
+    execSync(`npx prisma generate ${schemaFlags}`, {
       encoding: 'utf-8',
-    }) // the default is 'buffer'
+    })
+    // migrate DB
+    execSync('rm -rf migrations', { encoding: 'utf-8' })
+    execSync(`npx prisma migrate reset --force ${schemaFlags}`, {
+      encoding: 'utf-8',
+    })
+    execSync(`npx prisma migrate dev --name init ${schemaFlags}`, {
+      encoding: 'utf-8',
+    })
 
     // read from json file
     const text = fs.readFileSync('json-schema/json-schema.json', 'utf8')
     const jsonFile = JSON.parse(text)
     const models = Object.keys(jsonFile.definitions)
 
-    // update env DB
-    execSync('cp ./.env.example ./.env', { encoding: 'utf-8' })
-    updateEnv('./.env', 'DATABASE_URL', flags['database-url'])
+    // iterate models, for each key.properties, insert data
+    const results = []
+    for (const model of models) {
+      console.log('Model:', model)
+      console.log('========')
+      const propertiesObj = jsonFile.definitions[model].properties
+      const propertiesArr = Object.keys(propertiesObj)
 
-    // migrate DB
-    execSync('rm -rf migrations', { encoding: 'utf-8' })
-    execSync(`npx prisma migrate reset --force --schema=${flags.schema}`, {
-      encoding: 'utf-8',
-    })
-    const exec = execSync(
-      `npx prisma migrate dev --name init --schema=${flags.schema}`,
-      {
-        encoding: 'utf-8',
+      const fakeDatas: any = []
+      for (let index = 0; index < 3; index++) {
+        const fake: any = Object.assign(
+          {},
+          ...propertiesArr
+            .filter(
+              (key) =>
+                key !== 'id' &&
+                (propertiesObj[key].type === 'integer' ||
+                  propertiesObj[key].type === 'string')
+            )
+            .map((key) => ({
+              [key]: propertiesObj[key].type === 'integer' ? 1 : 'lalala',
+            }))
+        )
+
+        fakeDatas.push(fake)
       }
-    )
-    console.log(exec)
 
-    // // iterate models, for each key.properties, insert data
-    // const results = []
-    // for (const model of models) {
-    //   console.log('========')
-    //   console.log('Model:', model)
-    //   console.log('========')
-    //   const propertiesObj = jsonFile.definitions[model].properties
-    //   const propertiesArr = Object.keys(propertiesObj)
+      const key = model.toLowerCase()
+      results.push(
+        prisma[(key as 'user') || (key as 'post')].createMany({
+          data: fakeDatas,
+        })
+      )
+    }
 
-    //   const fakeDatas: any = []
-
-    //   for (let index = 0; index < 3; index++) {
-    //     const fake: any = Object.assign(
-    //       {},
-    //       ...propertiesArr.map((key) => ({
-    //         [key]:
-    //           propertiesObj[key].type === 'integer'
-    //             ? Math.random().toString()
-    //             : 'lalala',
-    //       }))
-    //     )
-
-    //     fakeDatas.push(fake)
-    //   }
-
-    //   results.push(
-    //     // eslint-disable-next-line dot-notation
-    //     prisma['user'].createMany({
-    //       data: fakeDatas,
-    //     })
-    //   )
-    // }
-
-    // await Promise.all(results)
+    await Promise.all(results)
 
     await prisma.$disconnect()
     this.log('============================')
