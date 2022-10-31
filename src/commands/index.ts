8e88342833931c59ef1b2061f37bd6f2dffda9a7
Raosan Fikri <raosanf.lillahi@gmail.com>
2022-10-31 13:24:36 +0700

    Feat/write db from schema example file (#11)

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,9 +1,14 @@
-import {Command, Flags} from '@oclif/core'
-import {execSync} from 'node:child_process'
+import { Command, Flags } from '@oclif/core'
+import { execSync } from 'node:child_process'
 import * as fs from 'node:fs'
 import { updateEnv } from '../scripts/env-edit'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
 export default class PrismaSeeder extends Command {
-  static description = 'Start seeding by providing path_to_schema_prisma_file and url_of_the_database'
+  static description =
+    'Start seeding by providing path_to_schema_prisma_file and url_of_the_database'
 
   static examples = [
     `$ oex --schema <path_to_schema_prisma_file> --database-url <url_of_the_database>
@@ -13,38 +18,108 @@ Finish seeding.
   ]
 
   static flags = {
-    schema: Flags.string({char: 's', description: 'path to schema prisma file', required: true}),
-    'database-url': Flags.string({char: 'd', description: 'url of the database', required: true}),
+    schema: Flags.string({
+      char: 's',
+      description: 'path to schema prisma file',
+      required: true,
+    }),
+    'database-url': Flags.string({
+      char: 'd',
+      description: 'url of the database',
+      required: true,
+    }),
   }
 
   async run(): Promise<void> {
-    const {flags} = await this.parse(PrismaSeeder)
+    const { flags } = await this.parse(PrismaSeeder)
 
     this.log('Start seeding...')
     this.log('============================')
-    this.log(`The schema path: ${flags.schema} \nand the DB url: ${flags['database-url']}`)
+    this.log(
+      `The schema path: ${flags.schema} \nand the DB url: ${flags['database-url']}`
+    )
     this.log('============================')
 
+    // update env DB
+    this.log('Updating env...')
+    this.log('============================')
+    execSync('cp ./.env.example ./.env', { encoding: 'utf-8' })
+    updateEnv('./.env', 'DATABASE_URL', flags['database-url'])
+
+    // copy prisma schema file
+    this.log('Copying schema file...')
+    this.log('============================')
+    execSync('rm -rf prisma', { encoding: 'utf-8' })
+    execSync('mkdir prisma', { encoding: 'utf-8' })
+    execSync(`cp ${flags.schema} ./prisma/schema.prisma`, { encoding: 'utf-8' })
+
     // generate json file
-    execSync(`npx prisma generate --schema=${flags.schema}`, {encoding: 'utf-8'})  // the default is 'buffer'
+    this.log('Generating prisma schema...')
+    this.log('============================')
+    execSync(`npx prisma generate`, { encoding: 'utf-8' })
+
+    // migrate DB
+    this.log('Resetting DB...')
+    this.log('============================')
+    execSync('rm -rf migrations', { encoding: 'utf-8' })
+    execSync(`npx prisma migrate reset --force --skip-generate`, {
+      encoding: 'utf-8',
+    })
+    this.log('Migrating DB...')
+    this.log('============================')
+    execSync(`npx prisma migrate dev --name init`, { encoding: 'utf-8' })
 
     // read from json file
-    const text = fs.readFileSync('json-schema/json-schema.json', 'utf8')
+    const text = fs.readFileSync('prisma/json-schema/json-schema.json', 'utf8')
     const jsonFile = JSON.parse(text)
     const models = Object.keys(jsonFile.definitions)
 
-    // update env DB
-    execSync('cp ./.env.example ./.env', {encoding: 'utf-8'})
-    updateEnv('./.env', 'DATABASE_URL', flags['database-url'])
-
-    // migrate DB
-    execSync(`npx prisma migrate deploy --schema=${flags.schema}`, {encoding: 'utf-8'})
-
-    // iterate models, for each key.properties, insert data
+    // iterate models, for each models.properties, insert data
+    const results = []
     for (const model of models) {
-      console.log(model)
+      this.log('Seeding model ' + model + '...')
+      this.log('============================')
+      const propertiesObj = jsonFile.definitions[model].properties
+      const propertiesArr = Object.keys(propertiesObj)
+
+      const fakeDatas: any = []
+      for (let index = 0; index < 3; index++) {
+        const fake: any = Object.assign(
+          {},
+          ...propertiesArr
+            .filter(
+              (key) =>
+                key !== 'id' &&
+                (propertiesObj[key].type === 'integer' ||
+                  propertiesObj[key].type === 'string' ||
+                  propertiesObj[key].type === 'boolean')
+            )
+            .map((key) => ({
+              [key]:
+                propertiesObj[key].type === 'integer'
+                  ? 1
+                  : propertiesObj[key].type === 'boolean'
+                  ? true
+                  : 'random string',
+            }))
+        )
+
+        fakeDatas.push(fake)
+      }
+
+      const key = model.toLowerCase()
+      results.push(
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        prisma[key].createMany({
+          data: fakeDatas,
+        })
+      )
     }
 
+    await Promise.all(results)
+
+    await prisma.$disconnect()
     this.log('============================')
     this.log('Finish seeding.')
   }
