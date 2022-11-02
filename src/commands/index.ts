28d94f20fec198fa251d5af14cebf46b41c2be18
raosan <raosanf.lillahi@gmail.com>
2022-11-02 15:31:17 +0700

    dynamic import prisma client

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,10 +2,9 @@ import { Command, Flags } from '@oclif/core'
 import { execSync } from 'node:child_process'
 import * as fs from 'node:fs'
 import { updateEnv } from '../scripts/env-edit'
-import { PrismaClient } from '@prisma/client'
 import { v4 as uuidv4 } from 'uuid'
 
-const prisma = new PrismaClient()
+let prisma: any
 const results: any[] = []
 
 export default class PrismaSeeder extends Command {
@@ -30,55 +29,60 @@ Finish seeding.
       description: 'url of the database',
       required: true,
     }),
-    execute: Flags.string({
-      char: 'e',
-      description: 'execute type. raw or not',
-    }),
   }
 
   async run(): Promise<void> {
     const { flags } = await this.parse(PrismaSeeder)
 
     this.log('Start seeding...')
-    this.log('============================')
     this.log(
       `The schema path: ${flags.schema} \nand the DB url: ${flags['database-url']}`
     )
-    this.log('============================')
 
     // update env DB
     this.log('Updating env...')
-    this.log('============================')
     execSync('cp ./.env.example ./.env')
     updateEnv('./.env', 'DATABASE_URL', flags['database-url'])
 
     // copy prisma schema file
     this.log('Copying schema file...')
-    this.log('============================')
     execSync('rm -rf prisma')
     execSync('mkdir prisma')
     execSync(`cp ${flags.schema} ./prisma/schema.prisma`)
 
     // generate json file and prisma client
     this.log('Generating prisma schema...')
-    this.log('============================')
     execSync(`npx prisma generate`)
 
-    // migrate DB
+    // reset DB
     this.log('Resetting DB...')
-    this.log('============================')
     execSync('rm -rf migrations')
     execSync(`npx prisma migrate reset --force --skip-generate`)
+
+    // migrate DB
     this.log('Migrating DB...')
-    this.log('============================')
-    execSync(`npx prisma migrate dev --name init`)
+    console.time('migration finish in:')
+    // TODO: can't decide should we use db push or migrate dev.
+    // --- execSync(`npx prisma migrate dev --name init`) ---
+    // which one will have effect on later use?
+    // for now, we use db push because it is faster by 1/3 time
+    execSync(`npx prisma db push`)
+    console.timeEnd('migration finish in:')
+
+    // get new generated prisma client
+    this.log('Getting new Prisma Client...')
+    // eslint-disable-next-line unicorn/prefer-module
+    const { PrismaClient } = require('@prisma/client')
+    prisma = new PrismaClient()
 
     // read from json file
+    this.log('Reading json schema...')
     const text = fs.readFileSync('prisma/json-schema/json-schema.json', 'utf8')
     const jsonFile = JSON.parse(text)
     const models = Object.keys(jsonFile.definitions)
 
     // iterate models, for each models.properties, insert to results array
+    this.log('Generating fake data...')
     const generatedUUID = uuidv4()
     for (const model of models) {
       const propertiesObj = jsonFile.definitions[model].properties
@@ -121,63 +125,29 @@ Finish seeding.
       const item = results[i]
       if (item) {
         // eslint-disable-next-line no-await-in-loop
-        await insertData(i, item, flags)
+        await insertData(i, item)
       }
 
       i += 1
     }
 
     await prisma.$disconnect()
-    this.log('============================')
     this.log('Finish seeding.')
   }
 }
 
-const isPascalCase = (word: string) => {
-  const pattern = /^[A-Z][A-Za-z]*$/
-  return pattern.test(word)
-}
-
-const camelToSnakeCase = (str: string) => {
-  const formattedStr = str.replace('ID', 'Id')
-  return formattedStr.replace(/[A-Z]/g, (letter) => `_${letter.toLowerCase()}`)
-}
-
-async function insertData(index: number, data: any, flags: any) {
+async function insertData(index: number, data: any) {
   const { $model, ...cleanData } = data
 
   try {
     console.log('Seeding model ' + $model + '...')
 
-    if (flags.execute) {
-      // Execute raw sql
-      const model = isPascalCase($model) ? $model : camelToSnakeCase($model)
-      const keys = Object.keys(cleanData)
-      const keysColumns = keys
-        .map((item: string) =>
-          isPascalCase($model) ? `"${item}"` : `"${camelToSnakeCase(item)}"`
-        )
-        .join(', ')
-      const values = keys
-        .map((item: string) =>
-          cleanData[item]?.length > 10
-            ? `'${cleanData[item]}'`
-            : cleanData[item]
-        )
-        .join(', ')
+    // Seed using prisma client
+    const key = $model[0].toLowerCase() + $model.slice(1)
 
-      await prisma.$executeRawUnsafe(
-        `INSERT INTO "${model}" (${keysColumns})
-        VALUES (${values});`
-      )
-    } else {
-      // Seed using prisma client
-      const key = $model[0].toLowerCase() + $model.slice(1)
-
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      await prisma[key].create({ data: cleanData })
-    }
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    await prisma[key].create({ data: cleanData })
 
     results[index] = null
   } catch (error: any) {
