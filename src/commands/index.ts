bb49697946138c76ba482cf5c8f0f19c023249c9
Raosan Fikri <raosanf.lillahi@gmail.com>
2022-11-02 15:55:36 +0700

    Feat/adding config programatically and and dynamic import prisma client (#14)

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,10 +2,9 @@ import { Command, Flags } from '@oclif/core'
 import { execSync } from 'node:child_process'
 import * as fs from 'node:fs'
 import { updateEnv } from '../scripts/env-edit'
-import { PrismaClient } from '@prisma/client'
 import { v4 as uuidv4 } from 'uuid'
 
-const prisma = new PrismaClient()
+let prisma: any
 const results: any[] = []
 
 export default class PrismaSeeder extends Command {
@@ -36,47 +35,64 @@ Finish seeding.
     const { flags } = await this.parse(PrismaSeeder)
 
     this.log('Start seeding...')
-    this.log('============================')
     this.log(
       `The schema path: ${flags.schema} \nand the DB url: ${flags['database-url']}`
     )
-    this.log('============================')
 
     // update env DB
     this.log('Updating env...')
-    this.log('============================')
-    execSync('cp ./.env.example ./.env', { encoding: 'utf-8' })
+    execSync('cp ./.env.example ./.env')
     updateEnv('./.env', 'DATABASE_URL', flags['database-url'])
 
     // copy prisma schema file
     this.log('Copying schema file...')
-    this.log('============================')
-    execSync('rm -rf prisma', { encoding: 'utf-8' })
-    execSync('mkdir prisma', { encoding: 'utf-8' })
-    execSync(`cp ${flags.schema} ./prisma/schema.prisma`, { encoding: 'utf-8' })
+    execSync('rm -rf prisma')
+    execSync('mkdir prisma')
+    execSync(`cp ${flags.schema} ./prisma/schema.prisma`)
 
-    // generate json file
+    // insert json schema generator config
+    this.log('Inserting json generator config...')
+    const generatorConfig = `\r\r
+generator jsonSchema {
+  provider = "prisma-json-schema-generator"
+  keepRelationScalarFields = "true"
+  includeRequiredFields = "true"
+}`
+    fs.appendFileSync('./prisma/schema.prisma', generatorConfig)
+
+    // generate json file and prisma client
     this.log('Generating prisma schema...')
-    this.log('============================')
-    execSync(`npx prisma generate`, { encoding: 'utf-8' })
+    execSync(`npx prisma generate`)
+
+    // reset DB
+    this.log('Resetting DB...')
+    execSync('rm -rf migrations')
+    execSync(`npx prisma migrate reset --force --skip-generate`)
 
     // migrate DB
-    this.log('Resetting DB...')
-    this.log('============================')
-    execSync('rm -rf migrations', { encoding: 'utf-8' })
-    execSync(`npx prisma migrate reset --force --skip-generate`, {
-      encoding: 'utf-8',
-    })
     this.log('Migrating DB...')
-    this.log('============================')
-    execSync(`npx prisma migrate dev --name init`, { encoding: 'utf-8' })
+    console.time('Migration finish in')
+    // TODO: can't decide should we use db push or migrate dev.
+    // --- execSync(`npx prisma migrate dev --name init`) ---
+    // which one will have effect on later use?
+    // for now, we use db push because it is faster by 1/3 time
+    execSync(`npx prisma db push`)
+    console.timeEnd('Migration finish in')
+
+    // get new generated prisma client
+    this.log('Getting new Prisma Client...')
+    // eslint-disable-next-line unicorn/prefer-module
+    const { PrismaClient } = require('@prisma/client')
+    prisma = new PrismaClient()
 
     // read from json file
+    this.log('Reading json schema...')
     const text = fs.readFileSync('prisma/json-schema/json-schema.json', 'utf8')
     const jsonFile = JSON.parse(text)
     const models = Object.keys(jsonFile.definitions)
 
     // iterate models, for each models.properties, insert to results array
+    this.log('Generating fake data...')
     const generatedUUID = uuidv4()
     for (const model of models) {
       const propertiesObj = jsonFile.definitions[model].properties
@@ -126,7 +142,6 @@ Finish seeding.
     }
 
     await prisma.$disconnect()
-    this.log('============================')
     this.log('Finish seeding.')
   }
 }
@@ -136,11 +151,14 @@ async function insertData(index: number, data: any) {
 
   try {
     console.log('Seeding model ' + $model + '...')
+
+    // Seed using prisma client
     const key = $model[0].toLowerCase() + $model.slice(1)
 
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     await prisma[key].create({ data: cleanData })
+
     results[index] = null
   } catch (error: any) {
     if (error.code === 'P2003') {
@@ -150,9 +168,10 @@ async function insertData(index: number, data: any) {
           ' due to [FKey Constarint]. Will be retried'
       )
       results.push(data)
-      results[index] = null
     } else {
       console.log(error)
     }
+
+    results[index] = null
   }
 }
